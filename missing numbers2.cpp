2b140bb4339c6400a6a5d9ce0e3e9a59e0e6610f
REETESH TOMAR <54767198+15379180@users.noreply.github.com>
2020-02-16 11:12:54 -0800

    Add files via upload

---
+++ missing numbers2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    int arr[a];
+    for(int i=0;i<a;++i)
+        cin>>arr[i];
+    int b;
+    cin>>b;
+    int brr[b];
+    for(int  i=0;i<b;++i)
+        cin>>brr[i];
+
+    int h[200000]={};
+    for(int m=0;m<a;++m)
+    {
+        h[arr[m]]++;
+    }
+    int k[200000]={};
+    for(int n=0;n<b;++n)
+    {
+        k[brr[n]]++;
+    }
+    for(int n=0;n<200000;++n)
+    {
+        if(h[n]<k[n])
+            cout<<n<<" ";
+    }
+
+    return 0;
+}
+
+
